7ce4f839aa9d98b3bd2bfd038bdf446a798741d9
Zeynep Alpa <zeynep.alpa@istun.edu.tr>
2023-08-22 11:42:13 +0300

    installation completed successfully

---
+++ GameProject/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int strawberries = 0;
+    [SerializeField] private Text strawberriesText;
+    [SerializeField] private AudioSource collectionSoundEffect;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Strawberry"))
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            strawberries++;
+            strawberriesText.text="Strawberries: " + strawberries;
+        }
+    }
+}
